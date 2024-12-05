380c294712b04402026be0d70a2cd7c6cb3fe457
Bruno Gomes <132568509+BrnGomes0@users.noreply.github.com>
2024-12-05 09:53:53 -0300

    Update DeleteController.java

--- src/main/java/mrp_simulator/api/controller/DeleteController.java
+++ src/main/java/mrp_simulator/api/controller/DeleteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/delete")
+@CrossOrigin(origins = "https://posautomation.vercel.app")
 public class DeleteController {
 
     @Autowired
